763009ea75502dee7b43a1b9f4cf5f8319540c2a
cloudnexuss <cloudnexus@googlegroups.com>
2025-05-27 23:15:26 +0530

    hello

--- src/main/java/com/example/isms/config/WebConfig.java
+++ src/main/java/com/example/isms/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(new String[]{"http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://suchnasangam.s3-website.ap-south-1.amazonaws.com", "https://d3ohu1jvpua37v.cloudfront.net","https://isms-vedk.onrender/","https://isms-vedk.onrender"}).allowedMethods(new String[]{"GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"}).allowedHeaders(new String[]{"*"}).exposedHeaders(new String[]{"Authorization", "Content-Type"}).allowCredentials(true);
+                registry.addMapping("/**").allowedOrigins(new String[]{"http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://suchnasangam.s3-website.ap-south-1.amazonaws.com", "https://d3ohu1jvpua37v.cloudfront.net","https://isms-vedk.onrender.com/","https://isms-vedk.onrender.com"}).allowedMethods(new String[]{"GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"}).allowedHeaders(new String[]{"*"}).exposedHeaders(new String[]{"Authorization", "Content-Type"}).allowCredentials(true);
             }
         };
     }
